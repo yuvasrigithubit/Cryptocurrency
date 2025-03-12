d6520d28c24a2c3404923f0e7481939057c2d144
yuvasrigithubit <145039343+yuvasrigithubit@users.noreply.github.com>
2025-03-12 09:31:03 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Cryptocurrencies } from "./Cryptocurrencies.jsx";
-export { default as CryptoDetails } from "./CryptoDetails.jsx";
-
-export { default as Home } from "./Home.jsx";
